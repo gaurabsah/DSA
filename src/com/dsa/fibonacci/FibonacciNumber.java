b8c75a497f19c9e6f482475223473bc3c5fb018f
Gaurab Sah <gaurab.sah.777@gmail.com>
2024-12-10 15:30:07 +0545

    fibonacci

---
+++ src/com/dsa/fibonacci/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package com.dsa.fibonacci;
+
+// print the fibonacci number upto nth position 
+public class FibonacciNumber {
+
+	static void fibo(int n) {
+		int num1 = 0;
+		int num2 = 1;
+
+		for (int i = 0; i <= n; i++) {
+			System.out.print(num1 + " ");
+			int num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 8;
+		fibo(n);
+
+	}
+
+}
---
+++ src/com/dsa/fibonacci/recursion/FibonacciNumber.java
@@ -0,0 +1,26 @@
+package com.dsa.fibonacci.recursion;
+
+// 0 1 1 2 3 5 8 13 21 ...
+// find the number at nth index (0 indexing)
+
+public class FibonacciNumber {
+
+	/*
+	 * TC - > O(2^n)
+	 * SC -> O(n)
+	 */
+	static int fibo(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+
+		return fibo(n - 1) + fibo(n - 2);
+	}
+
+	public static void main(String[] args) {
+		int n = 5;
+		int fibo = fibo(n);
+		System.out.println(fibo);
+	}
+
+}
